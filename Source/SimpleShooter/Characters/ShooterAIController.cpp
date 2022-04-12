6eba38c9f52aa5591494e3b50b12e85bb9a632d1
Asraf <noorworkz@gmail.com>
2022-04-12 16:59:25 +0530

    main files

---
+++ Source/SimpleShooter/Characters/ShooterAIController.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SimpleShooter/Characters/ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "ShooterCharacter.h"
+
+void AShooterAIController::BeginPlay() 
+{
+    Super::BeginPlay();
+
+
+    if (AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+
+        APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+
+    }    
+}
+ 
+ void AShooterAIController::Tick(float DeltaSeconds) 
+ {
+    Super::Tick(DeltaSeconds);
+
+// **** this code is replaced by the BTSERVICES C++ inside the BT_ENEMEYAI editor  ****
+    // APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    // if (LineOfSightTo(PlayerPawn))
+    // {
+    //     // SetFocus(PlayerPawn); CUSTOM AI
+    //     // MoveToActor(PlayerPawn, AcceptanceRadius);
+
+    //     GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+    //     GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+    // }
+    // else
+    // {
+    //     // ClearFocus(EAIFocusPriority::Gameplay); CUSTOM AI
+    //     // StopMovement();
+
+    //     GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+    // }
+ }
+ 
+ bool AShooterAIController::IsDead() const
+ {
+    AShooterCharacter* ControlledCharacter = Cast<AShooterCharacter>(GetPawn());
+    if (ControlledCharacter != nullptr)
+    {
+        return ControlledCharacter->IsDead();
+    }
+
+    return true;
+ }
+ 
